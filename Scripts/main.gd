73844d0b036e8cb9489fe133d33084c9ff2d6f86
harshborkar <harshborkar1@gmail.com>
2025-08-27 01:43:58 +0530

    Add Barrel scene and improve scene structure
    Added a new Barrel scene with associated assets and integrated it into the main scene under a new Actors node. Updated Player and Barrel collision layers and masks, improved Player.tscn structure, and refactored main.gd to support the new node hierarchy. Enhanced player.gd with attack state logic and utility functions. Updated project.godot with named physics layers and rendering settings.

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,15 +1,15 @@
 extends Node2D
-@onready var player: CharacterBody2D = $Player
+@onready var player: CharacterBody2D = $Actors/Player
+
 
 @onready var camera_2d: Camera2D = $Camera2D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if player.global_position.x >camera_2d.global_position.x:
-		camera_2d.global_position.x = player.global_position.x
+	if player.position.x > camera_2d.position.x:
+		camera_2d.position.x = player.position.x
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -15,20 +15,43 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 func handle_input()->void:
+	
 	var direction:=Input.get_vector("Left", "Right","Up", "Down" )
 	velocity = direction*SPEED
 	
+	if can_attack() and Input.is_action_just_pressed("Attack"):
+		state= STATE.ATTACK
+		#""""""""""""""animation logic""""""""""
+		handle_animations()
+	
 func handle_movement()->void:
-	if velocity.length() == 0:
-		state = STATE.IDLE
+	if can_move():
+		if velocity.length() == 0:
+			state = STATE.IDLE
+		else:
+			state = STATE.WALK
 	else:
-		state = STATE.WALK
+		velocity = Vector2.ZERO
 		
 func handle_animations()->void:
 	if state==STATE.IDLE:
 		pass
-	if state==STATE.WALK:
+	elif state==STATE.WALK:
 		pass
+	elif state==STATE.ATTACK:
+		pass
+		
+func can_move()->bool:
+	return state==STATE.IDLE or state == STATE.WALK
+	
+func can_attack()->bool:
+	return state == STATE.IDLE or state==STATE.WALK
+	
+	
 	
 func handle_flip():
 	pass
+
+func on_action_completed()->void:
+	state=STATE.IDLE
+	
