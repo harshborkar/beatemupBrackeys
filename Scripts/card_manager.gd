6ddd7d2a32f834c35cdfb9f6e1c6c5eb8a4682a7
harshborkar <harshborkar1@gmail.com>
2025-08-29 20:37:20 +0530

    aa

--- Scripts/card_manager.gd
+++ Scripts/card_manager.gd
@@ -1,2 +1,19 @@
 class_name card_manager
 extends Node2D
+
+
+
+
+func connect_card_signal(Card:card):
+	Card.hovered_on.connect(on_hover.bind())
+	Card.hovered_off.connect(off_hover.bind())
+
+
+func on_hover(Card:card):
+	Card.on_hover()
+	if Input.is_action_just_pressed("Left_Click"):
+		Card.flip_card()
+	
+
+func off_hover(Card:card):
+	Card.off_hover()
--- Scripts/cards.gd
+++ Scripts/cards.gd
@@ -1,7 +1,35 @@
 class_name card
 extends Node2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
+signal hovered_on
+signal hovered_off
+
+func _ready() -> void:
+	# Create a unique material instance for each card
+	sprite_2d.material = sprite_2d.material.duplicate()
+	get_parent().connect_card_signal(self)
 
 func flip_card():
+	print("flip")
 	animation_player.play("Flip")
+
+func on_hover():
+	var tween = get_tree().create_tween()
+	tween.tween_method(set_rand_trans_power, 1.0, 3.0, 0.2)
+	if Input.is_action_just_pressed("Left_Click"):
+		flip_card()
+
+func off_hover():
+	var tween = get_tree().create_tween()
+	tween.tween_method(set_rand_trans_power, 3.0, 1.0, 0.2)
+
+func set_rand_trans_power(value: float):
+	sprite_2d.material.set_shader_parameter("rand_trans_power", value)
+
+func _on_area_2d_mouse_entered() -> void:
+	emit_signal("hovered_on", self)
+
+func _on_area_2d_mouse_exited() -> void:
+	emit_signal("hovered_off", self)
